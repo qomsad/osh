2d2f69dcbf07b1ecc52d5087b73545f540262a08
qomsad <149834513+qomsad@users.noreply.github.com>
2024-08-19 22:47:37 +0500

    Инициализация сервиса администратора

---
+++ packages/osh-service-admin/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
+
+import "dotenv/config";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env["PORT"] || 3000);
+}
+bootstrap();
